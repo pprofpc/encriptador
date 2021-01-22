dd0bc82697cf1d14ae4b6465523271fe44475a44
Pablo CANEPA <pprofpc@gmail.com>
2021-01-22 15:56:08 -0300

    Comienzo pestaña desencriptar

--- main.py
+++ main.py
@@ -19,9 +19,15 @@ password = StringVar()
 mostrarDato = StringVar()
 estadoArchivo = StringVar()
 estadoArchivo.set("Cargar archivo")
-
 archivoLeer = Archivo()
 
+#Variables Desencriptador
+
+passwordD = StringVar()
+mostrarDatoD = StringVar()
+estadoArchivoD = StringVar()
+estadoArchivoD.set("Cargar archivo")
+archivoLeerD = Archivo()
 #-----Lógica del programa----
 
 #Lógica del Botón Encriptar
@@ -69,6 +75,28 @@ def guardar_archivo():
                 i += 1
             file.close()
             Messagebox.showinfo("Info", "Datos Guardados!")
+
+#----Lógica de la pestaña Desencriptar
+
+
+def setDatoD():
+    return True
+
+
+def abrir_archivoD():
+    ruta_app = os.path.abspath("./")
+    archivo_abierto = filedialog.askopenfile(initialdir=ruta_app, title= "Seleccionar archivo",filetypes = (("TXT","*.txt"),("All Files","*.*")))
+    if archivo_abierto is None:
+        Messagebox.showwarning("Atención", "Cancelo la operación!")
+    else:
+        archivoLeerD.setCompleto(archivo_abierto.name)
+        if archivoLeerD.getOk():
+            estadoArchivoD.set("Archivo Cargado")
+            btnDesencriptar['state'] = 'normal'
+
+
+def guardar_archivoD():
+    return True
 
 #----Geometría de la UI----
 window.title("Encriptador")
@@ -123,6 +151,48 @@ btnGuardar = Button(tab1, text="Guardar datos encriptados",
                     state=DISABLED)
 
 btnGuardar.grid(row=9, columnspan=2, column=0, sticky=E)
+
+#----Pestaña Desencriptar----
+
+lblSeparadorD = Label(tab2)
+lblSeparadorD.grid(column=0, row=0)
+
+btnBuscarD = Button(tab2, text="Buscar archivo a desencriptar",
+                   command=abrir_archivoD)
+btnBuscarD.grid(row=1, columnspan=2, column=0, sticky=E)
+
+lblArchivoOkD = Label(tab2, textvariable=estadoArchivoD)
+lblArchivoOkD.grid(row=1, column=2)
+
+lblPasswordD = Label(tab2, text="Contraseña: ")
+lblPasswordD.grid(row=3, column=0)
+
+txbPasswordD = Entry(tab2, textvariable=passwordD, show="**")
+txbPasswordD.grid(row=3, column=1)
+
+lblBanderaPasswordD = Label(tab2, textvariable=mostrarDatoD)
+lblBanderaPasswordD.grid(row=4, column=0, columnspan=2)
+
+#Botón Encriptar
+
+btnDesencriptar = Button(tab2, text="Desencriptar", command=setDatoD, state=DISABLED)
+btnDesencriptar.grid(row=5, column=1, sticky=E, pady=10)
+
+#----------Espacio de guardado--------------------
+
+lineaGuardarD = ttk.Separator(tab2, orient='horizontal')
+#lineaGuardar.place(relx=0, rely=0.33, relwidth=1, relheight=1)
+lineaGuardarD.grid(row=7, column=0, columnspan=4, sticky='ew')
+lblSeparador2D = Label(tab2)
+lblSeparador2D.grid(column=0, row=8)
+
+#Botón Guardar
+btnGuardarD = Button(tab2, text="Guardar datos desencriptados",
+                    command=guardar_archivoD,
+                    state=DISABLED)
+
+btnGuardarD.grid(row=9, columnspan=2, column=0, sticky=E)
+
 
 #----Empaquetado Final----
 
